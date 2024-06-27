810e245f3c367924cf9e06d77e0a8ab409778675
theagentpenguin <gunaseelanabcd@gmail.com>
2024-06-27 22:01:57 +0530

    first commit

---
+++ todo/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      ToDo App
+      <div className="main-body">
+        <input type="text"></input>
+      </div>
+    </div>
+  );
+}
+
+export default App;
