f4e6895d9e894365090d22de7c431663fb552ef8
theagentpenguin <gunaseelanabcd@gmail.com>
2024-06-29 17:38:42 +0530

    made few changes

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,14 +1,35 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [newData, setNewData] = useState("");
+  const [todos, setTodos] = useState([]);
+  function handleSubmit(e){
+    e.preventDefault()
+    setTodos(currentTodos => {
+      return [
+        ...todos, {title: newData}
+      ]
+    });
+  }
+  console.log(todos);
   return (
+    <>
+    <form onSubmit={handleSubmit}>
     <div className="App">
       ToDo App
+      
       <div className="main-body">
-        <input type="text"></input>
+
+        <input type="text" value={newData} onChange={e => setNewData(e.target.value)} id='inputData'></input>
+        <button className="btn">Add</button>
       </div>
+      
     </div>
+    </form>
+    </>
   );
 }
 
